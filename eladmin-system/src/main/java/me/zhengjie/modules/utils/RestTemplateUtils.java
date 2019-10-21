50b75aa37b0db491e91809707e60208b1ff7f093
Hadoop <1011161615@qq.com>
2019-10-21 11:55:20 +0800

    修改添加工具类restTemplateUtils

--- eladmin-system/src/main/java/me/zhengjie/modules/utils/RestTemplateUtils.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/utils/RestTemplateUtils.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -26,17 +28,17 @@ public class RestTemplateUtils {
         return  restTemplate.exchange(url, HttpMethod.GET, requestEntity, HashMap.class).getBody();
     }
 
-    public Object sendPost(String url,String Token){
+    public Object sendPost(String url,String Token,LinkedMultiValueMap body){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer "+Token);
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body,headers);
         return  restTemplate.exchange(url, HttpMethod.POST, requestEntity, HashMap.class).getBody();
     }
 
-    public Object sendPut(String url,String Token){
+    public Object sendPut(String url,String Token,LinkedMultiValueMap body){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer "+Token);
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body,headers);
         return  restTemplate.exchange(url, HttpMethod.PUT, requestEntity, HashMap.class).getBody();
     }
 
